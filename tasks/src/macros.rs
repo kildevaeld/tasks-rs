ae9523c163eec298c1e52b81ca6a1f953d44f9e3
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2019-09-26 23:24:38 +0200

    more macros

--- tasks/src/macros.rs
+++ tasks/src/macros.rs
@@ -18,3 +18,100 @@ macro_rules! middleware_fn {
         $crate::middleware::MiddlewareFn::new($handler)
     };
 }
+
+
+#[macro_export]
+macro_rules! pipe {
+    [ $y: expr, $( $x:expr ),* ] => {
+        {
+            use $crate::TaskExt;
+            let m = $y;
+            $(
+                let m = m.pipe($x);
+            )*
+            m
+        }
+     };
+
+     [ $y: expr] => {
+        {
+            $y
+        }
+     };
+}
+
+#[macro_export]
+macro_rules! chain {
+    [ $y: expr, $( $x:expr ),* ] => {
+        {
+            use $crate::ConditionalTaskExt;
+            let m = $y;
+            $(
+                let m = m.chain($x);
+            )*
+            m
+        }
+     };
+
+     [ $y: expr] => {
+        {
+            $y
+        }
+     };
+}
+
+
+#[macro_export]
+macro_rules! stack {
+    [ $y: expr, $( $x:expr ),* ] => {
+        {
+            use $crate::MiddlewareExt;
+            let m = $y;
+            $(
+                let m = m.stack($x);
+            )*
+            m
+        }
+     };
+
+     [ $y: expr] => {
+        {
+            $y
+        }
+     };
+}
+
+
+#[cfg(test)]
+mod tests {
+
+    // use super::super::station_fn;
+    // use super::super::Station;
+    use super::super::*;
+    use futures_util::future;
+    #[test]
+    fn test_pipe() {
+        let chain = pipe![
+            task_fn!(|s: &str| future::ok(s) ),
+            task_fn!(|s: &str| future::ok(s)),
+            pipe![task_fn!(|s: &str| future::ok(s))],
+            pipe![task_fn!(|s: &str| future::ok::<_, TaskError>(s))]
+        ];
+
+        let result = futures_executor::block_on(chain.exec("Hello, World!")).unwrap();
+        assert_eq!(result, "Hello, World!");
+    }
+
+    #[test]
+    fn test_conveyor_meaning_of_life() {
+        let chain = pipe![
+            task_fn!(|input: &str| future::ok(input.len())),
+            task_fn!(|len: usize| future::ok::<_, TaskError>(len * 7))
+        ];
+
+        let ans = futures_executor::block_on(chain.exec("Hello!"));
+
+        assert_eq!(ans.unwrap(), 42);
+    }
+
+}
