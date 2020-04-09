bf98bb770056a2480fbe3767c7acbaf4368abeb7
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2020-04-09 21:08:15 +0200

    reply

--- tasks-http/src/reply.rs
+++ tasks-http/src/reply.rs
@@ -1,4 +1,7 @@
+use super::modifiers::Header;
 use super::Response;
+use http::{HeaderValue, StatusCode};
+use modifier::Set;
 
 pub trait Reply {
     fn into_response(self) -> Response;
@@ -8,4 +11,21 @@ impl Reply for Response {
     fn into_response(self) -> Response {
         self
     }
+}
+
+pub struct Html<'a> {
+    body: &'a str,
+}
+
+impl<'a> Reply for Html<'a> {
+    fn into_response(self) -> Response {
+        Response::with(StatusCode::OK).set(self.body).set(Header(
+            "Content-Type",
+            HeaderValue::from_static("text/html"),
+        ))
+    }
+}
+
+pub fn html<'a>(body: &'a str) -> Html<'a> {
+    Html { body }
 }
