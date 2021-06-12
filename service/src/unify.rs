d5c6f62114dc950d1f9028969ae0c1af78f36a97
Rasmus Kildevæld <rasmuskildevaeld@gmail.com>
2021-06-12 22:40:06 +0200

    unify

--- service/src/unify.rs
+++ service/src/unify.rs
@@ -1,4 +1,4 @@
-use super::{Either, Service};
+use super::{Either, Rejection, Service};
 use core::future::Future;
 use core::marker::PhantomData;
 use core::pin::Pin;
@@ -11,43 +11,50 @@ pub struct Unify<F> {
     pub(super) filter: F,
 }
 
-impl<F, T, R> Service<R> for Unify<F>
+impl<F, T, E, R> Service<R> for Unify<F>
 where
-    F: Service<R, Output = Either<T, T>>,
+    F: Service<R, Output = Either<T, T>, Error = Either<E, E>>,
     R: Send,
+    E: Send,
 {
     type Output = T;
-    type Error = F::Error;
-    type Future = UnifyFuture<F::Future, R>;
+    type Error = E;
+    type Future = UnifyFuture<F::Future, R, E>;
     #[inline]
     fn call(&self, req: R) -> Self::Future {
         UnifyFuture {
             inner: self.filter.call(req),
             _r: PhantomData,
+            _e: PhantomData,
         }
     }
 }
 
 #[allow(missing_debug_implementations)]
 #[pin_project]
-pub struct UnifyFuture<F, R> {
+pub struct UnifyFuture<F, R, E> {
     #[pin]
     inner: F,
     _r: PhantomData<R>,
+    _e: PhantomData<E>,
 }
 
-impl<F, R, T> Future for UnifyFuture<F, R>
+impl<F, R, E, T> Future for UnifyFuture<F, R, E>
 where
-    F: TryFuture<Ok = Either<T, T>>,
+    F: TryFuture<Ok = Either<T, T>, Error = Rejection<R, Either<E, E>>>,
 {
-    type Output = Result<T, F::Error>;
+    type Output = Result<T, Rejection<R, E>>;
 
     #[inline]
     fn poll(self: Pin<&mut Self>, cx: &mut Context) -> Poll<Self::Output> {
         let unified = match ready!(self.project().inner.try_poll(cx)) {
             Ok(Either::A(a)) => Ok(a),
             Ok(Either::B(b)) => Ok(b),
-            Err(err) => Err(err),
+            Err(Rejection::Err(Either::A(a))) => Err(Rejection::Err(a)),
+            Err(Rejection::Err(Either::B(b))) => Err(Rejection::Err(b)),
+            Err(Rejection::Reject(req, Some(Either::A(a)))) => Err(Rejection::Reject(req, Some(a))),
+            Err(Rejection::Reject(req, Some(Either::B(b)))) => Err(Rejection::Reject(req, Some(b))),
+            Err(Rejection::Reject(req, None)) => Err(Rejection::Reject(req, None)),
         };
         Poll::Ready(unified)
     }
